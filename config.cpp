a3477e67f17b21f8142808114d95ec2739292e97
Ericu03 <44855495+Ericu03@users.noreply.github.com>
2022-02-07 15:27:38 +0900

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -10,13 +10,14 @@ const int BACK_RIGHT_WHEEL = 7;
 
 const int INTAKE_L = 7;
 const int INTAKE_R = 8;
-const int LIFT = 9;
-const int LIFT2 = 10;
+const int LIFT = 9;//front lift
+const int RING = 10;//ring
 
-const char LENC_TOP = 'F';
-const char LENC_BOT = 'B';
-const char RENC_TOP = 'G';
-const char RENC_BOT = 'H';
+const int VISION = 17;
+const char LENC_TOP = 'G';
+const char LENC_BOT = 'H';
+const char MID_TOP = 'E';
+const char MID_BOT = 'F';
 const char PNEUMATIC1 = 'D';
 const char PNEUMATIC2 = 'B';
 const char PNEUMATIC3 = 'C';
@@ -33,19 +34,23 @@ pros::Motor back_right_wheel(BACK_RIGHT_WHEEL, pros::E_MOTOR_GEARSET_18,false,pr
 pros::Motor intake_l(INTAKE_L,pros::E_MOTOR_GEARSET_18,true, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor intake_r(INTAKE_R,pros::E_MOTOR_GEARSET_18,false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor lift(LIFT, pros::E_MOTOR_GEARSET_18,true, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor lift2(LIFT2, pros::E_MOTOR_GEARSET_18,false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor ring(RING, pros::E_MOTOR_GEARSET_06,false, pros::E_MOTOR_ENCODER_DEGREES);
 pros::ADIEncoder left_enc (LENC_TOP, LENC_BOT, true);
-pros::ADIEncoder right_enc (RENC_TOP, RENC_BOT, false);
+pros::ADIEncoder mid_enc (MID_TOP, MID_BOT, false);
 pros::ADIDigitalOut pneumatic1 (PNEUMATIC1);
 pros::ADIDigitalOut pneumatic2 (PNEUMATIC2);
 pros::ADIDigitalOut pneumatic3 (PNEUMATIC3);
-
+pros::Vision vision (VISION);
 pros::Controller master(CONTROLLER_MASTER);
+
+
 
 //gui
 bool blueSide = false;
 bool close = false;
+bool skillsOn = false;
 int colorautonstate = 0;
 int sideautonstate = 0;
 bool colorbutton_unpressed = false;
 bool sidebutton_unpressed = false;
+int skillsbuttonstate = 0;
