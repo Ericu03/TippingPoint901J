3c18604247a916bbb5fd82ae812c080961cff9d0
Ericu03 <44855495+Ericu03@users.noreply.github.com>
2022-03-16 15:15:45 +0900

    Update auton_functions.h

--- auton_functions.h
+++ auton_functions.h
@@ -7,3 +7,7 @@ void turn_PID(float targetdegree);
 void move_straight_rel_test(double xCoord, int maxVel, int multi);
 void brakeMotor();
 void unbrakeMotor();
+void lift_PID(float targetDegree, int maxVelocity, int delay, int multi);
+void vision_tracker(int color, int direction);//1 or -1
+void pneumaticbacklift(bool choice);
+
